2ccd9e2e916515dcff3cf14a96dbf56ab4451f0c
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 01:53:24 +0700

    untuk menampilkan dialog konfirmasi ketika menghapus data

--- app/src/main/java/com/example/finalprojecta11/ui/view/buku/DetailBukuView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/buku/DetailBukuView.kt
@@ -1,5 +1,10 @@
 package com.example.finalprojecta11.ui.view.buku
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetail: DestinasiNavigasi {
@@ -7,4 +12,28 @@ object DestinasiDetail: DestinasiNavigasi {
     override val titleRes = "Detail Buku"
     const val id_buku = "id_buku"
     val routeWithArgs = "$route/{$id_buku}"
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah Anda Yakin Ingin Menghapus Data Ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
 }
