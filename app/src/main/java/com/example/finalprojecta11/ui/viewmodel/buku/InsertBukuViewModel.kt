fd709902b6b715f3237851283b879b6fb9430906
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-25 14:32:24 +0700

    untuk mengonversi objek InsertUiEvent menjadi objek Buku

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/buku/InsertBukuViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/buku/InsertBukuViewModel.kt
@@ -1,5 +1,15 @@
 package com.example.finalprojecta11.ui.viewmodel.buku
 
+import com.example.finalprojecta11.model.Buku
+
+fun InsertUiEvent.toBk(): Buku = Buku(
+    id_buku = id_buku,
+    judul = judul,
+    penulis = penulis,
+    kategori = kategori,
+    status = status
+)
+
 data class InsertUiEvent(
     val id_buku: String = "",
     val judul: String = "",
