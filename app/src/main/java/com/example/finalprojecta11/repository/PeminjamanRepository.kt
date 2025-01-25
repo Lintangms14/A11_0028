ceaef79de2ada58f9b2d48141d6c0fcf091637f9
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-25 13:12:20 +0700

    untuk mengelola data peminjaman dengan berinteraksi langsung ke API

--- app/src/main/java/com/example/finalprojecta11/repository/PeminjamanRepository.kt
+++ app/src/main/java/com/example/finalprojecta11/repository/PeminjamanRepository.kt
@@ -2,6 +2,8 @@ package com.example.finalprojecta11.repository
 
 import com.example.finalprojecta11.model.AllPeminjamanResponse
 import com.example.finalprojecta11.model.Peminjaman
+import com.example.finalprojecta11.service.PeminjamanService
+import okio.IOException
 
 interface PeminjamanRepository {
     suspend fun insertPeminjaman(peminjaman: Peminjaman)
@@ -13,4 +15,40 @@ interface PeminjamanRepository {
     suspend fun deletePeminjaman(id_peminjaman: String)
 
     suspend fun getPeminjamanByid(id_peminjaman: String): Peminjaman
+}
+
+class NetworkPeminjamanRepository(
+    private val peminjamanApiService: PeminjamanService
+) : PeminjamanRepository {
+
+    override suspend fun insertPeminjaman(peminjaman: Peminjaman) {
+        peminjamanApiService.insertPeminjaman(peminjaman)
+    }
+
+    override suspend fun updatePeminjaman(id_peminjaman: String, peminjaman: Peminjaman) {
+        peminjamanApiService.updatePeminjaman(id_peminjaman, peminjaman)
+    }
+
+    override suspend fun getPeminjaman(): AllPeminjamanResponse {
+        return peminjamanApiService.getAllPeminjaman()
+    }
+
+    override suspend fun deletePeminjaman(id_peminjaman: String) {
+        try {
+            val reponse = peminjamanApiService.deletePeminjaman(id_peminjaman)
+            if (!reponse.isSuccessful) {
+                throw IOException("Failed to delete kontak. HTTP Status code: " +
+                        "${reponse.code()}")
+            } else {
+                reponse.message()
+                println(reponse.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getPeminjamanByid(id_peminjaman: String): Peminjaman {
+        return peminjamanApiService.getPeminjamanByid(id_peminjaman).data
+    }
 }
