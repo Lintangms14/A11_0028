4582a09e2cbbd77fd48ccd60f6e0792157be12bd
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 02:00:58 +0700

    untuk menampilkan detail informasi buku

--- app/src/main/java/com/example/finalprojecta11/ui/view/buku/DetailBukuView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/buku/DetailBukuView.kt
@@ -1,10 +1,16 @@
 package com.example.finalprojecta11.ui.view.buku
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetail: DestinasiNavigasi {
@@ -12,6 +18,31 @@ object DestinasiDetail: DestinasiNavigasi {
     override val titleRes = "Detail Buku"
     const val id_buku = "id_buku"
     val routeWithArgs = "$route/{$id_buku}"
+}
+
+@Composable
+fun ComponentDetailBk(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+) {
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ){
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
 }
 
 @Composable
