224130799bba295ac8af8d7af9b9357f2228cdb6
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-25 22:25:07 +0700

    untuk mendefinisikan rute navigasi dan judul layar

---
+++ app/src/main/java/com/example/finalprojecta11/ui/view/buku/InsertBukuView.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojecta11.ui.view.buku
+
+import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Insert Buku"
+}
