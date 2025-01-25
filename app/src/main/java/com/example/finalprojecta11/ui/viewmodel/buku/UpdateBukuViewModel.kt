513d12270062ea6ee5cb6ccbd83138fb1c79f481
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-25 15:02:49 +0700

    untuk mengelola logika bisnis dan status UI terkait pembaruan data buku

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/buku/UpdateBukuViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/buku/UpdateBukuViewModel.kt
@@ -1,6 +1,54 @@
 package com.example.finalprojecta11.ui.viewmodel.buku
 
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.finalprojecta11.model.Buku
+import com.example.finalprojecta11.repository.BukuRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+
+class UpdateBukuViewModel(private val Bk: BukuRepository) : ViewModel() {
+    var UpdateuiState by mutableStateOf(UpdateUiState())
+        private set
+
+    fun updateState(updateUiEvent: UpdateUiEvent) {
+        UpdateuiState = UpdateUiState(updateUiEvent = updateUiEvent)
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun loadBuku(id_buku: String) {
+        viewModelScope.launch {
+            try {
+                val buku = Bk.getBukuByid(id_buku)
+                UpdateuiState = buku.toUpdateUiState()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: HttpException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun updateBk() {
+        viewModelScope.launch {
+            try {
+                val buku = UpdateuiState.updateUiEvent.toBk()
+                Bk.updateBuku(buku.id_buku, buku)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: HttpException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 fun Buku.toUpdateUiState(): UpdateUiState = UpdateUiState(
     updateUiEvent = toUpdateUiEvent()
