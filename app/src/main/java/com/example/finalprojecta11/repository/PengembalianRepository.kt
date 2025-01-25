7386f2dd7958b211e69eea4ef27632d5604f251c
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-25 13:25:13 +0700

    untuk mengelola data pengembalian melalui interaksi langsung dengan API

--- app/src/main/java/com/example/finalprojecta11/repository/PengembalianRepository.kt
+++ app/src/main/java/com/example/finalprojecta11/repository/PengembalianRepository.kt
@@ -2,6 +2,7 @@ package com.example.finalprojecta11.repository
 
 import com.example.finalprojecta11.model.AllPengembalianResponse
 import com.example.finalprojecta11.model.Pengembalian
+import com.example.finalprojecta11.service.PengembalianService
 
 interface PengembalianRepository {
     suspend fun insertPengembalian(pengembalian: Pengembalian)
@@ -9,4 +10,21 @@ interface PengembalianRepository {
     suspend fun getPengembalian(): AllPengembalianResponse
 
     suspend fun getPengembalianByid(id_pengembalian: String): Pengembalian
+}
+
+class NetworkPengembalianRepository(
+    private val pengembalianApiService: PengembalianService
+) : PengembalianRepository {
+
+    override suspend fun insertPengembalian(pengembalian: Pengembalian) {
+        pengembalianApiService.insertPengembalian(pengembalian)
+    }
+
+    override suspend fun getPengembalian(): AllPengembalianResponse {
+        return pengembalianApiService.getAllPengembalian()
+    }
+
+    override suspend fun getPengembalianByid(id_pengembalian: String): Pengembalian {
+        return pengembalianApiService.getPengembalianByid(id_pengembalian).data
+    }
 }
