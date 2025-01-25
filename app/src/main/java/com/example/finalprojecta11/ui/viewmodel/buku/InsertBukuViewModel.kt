0b984590d9c764494ad91f758beb9750e6fd2e76
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-25 14:38:23 +0700

    untuk mengelola dan mengontrol logika terkait proses penambahan buku

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/buku/InsertBukuViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/buku/InsertBukuViewModel.kt
@@ -1,6 +1,32 @@
 package com.example.finalprojecta11.ui.viewmodel.buku
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.finalprojecta11.model.Buku
+import com.example.finalprojecta11.repository.BukuRepository
+import kotlinx.coroutines.launch
+
+class InsertBukuViewModel(private val Bk: BukuRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertBkState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertBk(){
+        viewModelScope.launch {
+            try {
+                Bk.insertBuku(uiState.insertUiEvent.toBk())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 fun Buku.toUiStateBk(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
