c2285f6b130bf3b2639e748908b588c9d6418d92
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-25 14:54:38 +0700

    untuk mengonversi objek Buku menjadi objek UpdateUiEvent

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/buku/UpdateBukuViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/buku/UpdateBukuViewModel.kt
@@ -2,6 +2,14 @@ package com.example.finalprojecta11.ui.viewmodel.buku
 
 import com.example.finalprojecta11.model.Buku
 
+fun Buku.toUpdateUiEvent(): UpdateUiEvent = UpdateUiEvent(
+    id_buku = id_buku,
+    judul = judul,
+    penulis = penulis,
+    kategori = kategori,
+    status = status
+)
+
 fun UpdateUiEvent.toBk(): Buku = Buku(
     id_buku = id_buku,
     judul = judul,
