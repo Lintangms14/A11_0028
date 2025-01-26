976d4e8ffa346f798f44a7de575edfef00fd6d87
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 13:07:40 +0700

    untuk menampilkan detail dari objek Pengembalian di dalam sebuah Card

--- app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/DetailPengembalianView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/DetailPengembalianView.kt
@@ -1,21 +1,57 @@
 package com.example.finalprojecta11.ui.view.pengembalian
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.finalprojecta11.model.Peminjaman
+import com.example.finalprojecta11.model.Pengembalian
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+import com.example.finalprojecta11.ui.view.peminjaman.ComponentDetailAgt
 
 object DestinasiDetailPemngembalian: DestinasiNavigasi {
     override val route = "detail pengembalian"
     override val titleRes = "Detail Pengembalian"
     const val id_pengembalian = "id_pengembalian"
     val routeWithArgs = "$route/{$id_pengembalian}"
+}
+
+@Composable
+fun ItemDetailPngmbln(
+    modifier: Modifier = Modifier,
+    pengembalian: Pengembalian,
+) {
+    Card (
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ){
+            ComponentDetailAgt(judul = "Id Pengembalian", isinya = pengembalian.id_pengembalian)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailAgt(judul = "Id Peminjaman", isinya = pengembalian.id_peminjaman)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailAgt(judul = "Tanggal Di kembalikan", isinya = pengembalian.tanggal_dikembalikan)
+            Spacer(modifier = Modifier.padding(4.dp))
+    }
 }
 
 @Composable
