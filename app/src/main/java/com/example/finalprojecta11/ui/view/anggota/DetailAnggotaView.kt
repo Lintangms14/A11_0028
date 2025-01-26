17674730ac5c2189ab7bd3b78b75972f834728e0
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 11:56:20 +0700

    untuk menampilkan detail anggota dalam bentuk kartu

--- app/src/main/java/com/example/finalprojecta11/ui/view/anggota/DetailAnggotaView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/anggota/DetailAnggotaView.kt
@@ -1,8 +1,13 @@
 package com.example.finalprojecta11.ui.view.anggota
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -10,7 +15,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.finalprojecta11.model.Anggota
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetailAnggota: DestinasiNavigasi {
@@ -19,6 +26,38 @@ object DestinasiDetailAnggota: DestinasiNavigasi {
     const val id_anggota = "id_anggota"
     val routeWithArgs = "$route/{$id_anggota}"
 }
+
+@Composable
+fun ItemDetailAgt(
+    modifier: Modifier = Modifier,
+    anggota: Anggota,
+) {
+    Card (
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ){
+            ComponentDetailAgt(judul = "Id Anggota", isinya = anggota.id_anggota)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailAgt(judul = "Nama", isinya = anggota.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailAgt(judul = "Email", isinya = anggota.email)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailAgt(judul = "Nomor Telepon", isinya = anggota.nomor_telepon)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
+
 
 @Composable
 fun ComponentDetailAgt(
