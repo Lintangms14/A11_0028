bc77877c45458a25897421766df7c9419772ca6a
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 10:57:44 +0700

    untuk mengonversi objek UpdateAnggotaUiEvent menjadi objek Anggota

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/anggota/UpdateAnggotaViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/anggota/UpdateAnggotaViewModel.kt
@@ -1,5 +1,14 @@
 package com.example.finalprojecta11.ui.viewmodel.anggota
 
+import com.example.finalprojecta11.model.Anggota
+
+fun UpdateAnggotaUiEvent.toAgt(): Anggota = Anggota(
+    id_anggota = id_anggota,
+    nama = nama,
+    email = email,
+    nomor_telepon = nomor_telepon
+)
+
 data class UpdateAnggotaUiEvent(
     val id_anggota: String = "",
     val nama: String = "",
