ab481a50a28061420e1d357692b3075ea14ec921
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 11:48:47 +0700

    untuk menampilkan layar yang berfungsi untuk mengelola data anggota

---
+++ app/src/main/java/com/example/finalprojecta11/ui/view/anggota/InsertAnggotaView.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojecta11.ui.view.anggota
+
+import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntryAnggota: DestinasiNavigasi {
+    override val route = "item_anggota"
+    override val titleRes = "Insert Anggota"
+}
