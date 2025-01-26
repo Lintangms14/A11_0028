17eae488682e5359deff66e44172738ca8c6c044
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 11:08:35 +0700

    untuk menangani logika terkait dengan pembaruan data anggota

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/anggota/UpdateAnggotaViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/anggota/UpdateAnggotaViewModel.kt
@@ -1,6 +1,54 @@
 package com.example.finalprojecta11.ui.viewmodel.anggota
 
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.finalprojecta11.model.Anggota
+import com.example.finalprojecta11.repository.AnggotaRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+
+class UpdateAnggotaViewModel(private val Agt: AnggotaRepository) : ViewModel() {
+    var UpdateuiState by mutableStateOf(UpdateAnggotaUiState())
+        private set
+
+    fun updateState(updateUiEvent: UpdateAnggotaUiEvent) {
+        UpdateuiState = UpdateAnggotaUiState(updateAnggotaUiEvent = updateUiEvent)
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun loadAnggota(id_anggota: String) {
+        viewModelScope.launch {
+            try {
+                val anggota = Agt.getAnggotaByid(id_anggota)
+                UpdateuiState = anggota.toUpdateUiState()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: HttpException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun updateAgt() {
+        viewModelScope.launch {
+            try {
+                val anggota = UpdateuiState.updateAnggotaUiEvent.toAgt()
+                Agt.updateAnggota(anggota.id_anggota, anggota)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: HttpException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 fun Anggota.toUpdateUiState(): UpdateAnggotaUiState = UpdateAnggotaUiState(
     updateAnggotaUiEvent = toUpdateUiEvent()
