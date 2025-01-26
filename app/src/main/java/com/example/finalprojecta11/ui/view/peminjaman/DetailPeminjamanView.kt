1dddc0a2ef680d8aa898f8bdeaaa9b534af8c9ea
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:29:16 +0700

    untuk menampilkan detail informasi peminjaman dalam bentuk kartu

--- app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/DetailPeminjamanView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/DetailPeminjamanView.kt
@@ -1,8 +1,13 @@
 package com.example.finalprojecta11.ui.view.peminjaman
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -10,7 +15,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.finalprojecta11.model.Peminjaman
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetailPeminjaman: DestinasiNavigasi {
@@ -19,6 +26,41 @@ object DestinasiDetailPeminjaman: DestinasiNavigasi {
     const val id_peminjaman = "id_peminjaman"
     val routeWithArgs = "$route/{$id_peminjaman}"
 }
+
+@Composable
+fun ItemDetailPmjmn(
+    modifier: Modifier = Modifier,
+    peminjaman: Peminjaman,
+) {
+    Card (
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ){
+            ComponentDetailAgt(judul = "Id Peminjaman", isinya = peminjaman.id_peminjaman)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailAgt(judul = "Nama", isinya = peminjaman.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailAgt(judul = "Id Anggota", isinya = peminjaman.id_anggota)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailAgt(judul = "Tanggal Peminjaman", isinya = peminjaman.tanggal_peminjaman)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailAgt(judul = "Tanggal Pengembalian", isinya = peminjaman.tanggal_pengembalian)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
+
 
 @Composable
 fun ComponentDetailAgt(
