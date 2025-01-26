d86a08d64f04fcf4050f404dc1029be01d523691
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 11:43:32 +0700

    ntuk menampilkan daftar anggota dalam sebuah tampilan berbentuk tabel

--- app/src/main/java/com/example/finalprojecta11/ui/view/anggota/HomeAnggotaView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/anggota/HomeAnggotaView.kt
@@ -1,16 +1,22 @@
 package com.example.finalprojecta11.ui.view.anggota
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.finalprojecta11.model.Anggota
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
@@ -19,6 +25,58 @@ object DestinasiHomeAnggota : DestinasiNavigasi {
     override val route = "home anggota"
     override val titleRes = "Home Anggota"
 }
+
+@Composable
+fun AgtLayout(
+    anggota: List<Anggota>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Anggota) -> Unit
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .border(BorderStroke(1.dp, MaterialTheme.colorScheme.onSurfaceVariant))
+                .padding(vertical = 8.dp, horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Id Anggota",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(1f)
+            )
+            Text(
+                text = "Nama",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(2f)
+            )
+            Text(
+                text = "Email",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(2f)
+            )
+            Text(
+                text = "Nomor Telepon",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(2f)
+            )
+        }
+
+        LazyColumn(
+            contentPadding = PaddingValues(vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(anggota) { item ->
+                AgtTableRow(
+                    anggota = item,
+                    onDetailClick = onDetailClick
+                )
+            }
+        }
+    }
+}
+
 
 @Composable
 fun AgtTableRow(
