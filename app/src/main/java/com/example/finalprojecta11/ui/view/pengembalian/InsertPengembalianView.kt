eb0939c814065de575b6874031899c28c7226900
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:53:26 +0700

    untuk mendefinisikan halaman insert pengembalian

---
+++ app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/InsertPengembalianView.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojecta11.ui.view.pengembalian
+
+import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntryPengembalian: DestinasiNavigasi {
+    override val route = "item_pengembalian"
+    override val titleRes = "Insert Pengembalian"
+}
