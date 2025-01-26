f10cf7d589e5f2e823fe2a0f8e92f303f00def06
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:19:09 +0700

    untuk mendefinisikan rute dan judul untuk layar entri peminjaman

---
+++ app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/InsertPeminjamanView.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojecta11.ui.view.peminjaman
+
+import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntryPeminjaman: DestinasiNavigasi {
+    override val route = "item_peminjaman"
+    override val titleRes = "Insert Peminjaman"
+}
