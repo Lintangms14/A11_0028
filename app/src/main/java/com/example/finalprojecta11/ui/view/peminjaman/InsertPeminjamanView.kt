ecb66b8c41923afcda48d1b9643ac7e8e96ee3a2
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:21:58 +0700

    untuk menangani tampilan layar saat pengguna memasukkan data peminjaman baru

--- app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/InsertPeminjamanView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/InsertPeminjamanView.kt
@@ -4,21 +4,71 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalprojecta11.ui.customewidget.CostumeTopAppBar
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+import com.example.finalprojecta11.ui.viewmodel.PenyediaViewModel
 import com.example.finalprojecta11.ui.viewmodel.peminjaman.InsertPeminjamanUiEvent
 import com.example.finalprojecta11.ui.viewmodel.peminjaman.InsertPeminjamanUiState
+import com.example.finalprojecta11.ui.viewmodel.peminjaman.InsertPeminjamanViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntryPeminjaman: DestinasiNavigasi {
     override val route = "item_peminjaman"
     override val titleRes = "Insert Peminjaman"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryPmjmnScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertPeminjamanViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntryPeminjaman.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){
+            innerPadding ->
+        EntryBody(
+            insertPeminjamanUiState = viewModel.uiState,
+            onPeminjamanValueChange = viewModel::updateInsertPmjmnState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertPmjmn()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
 
 @Composable
