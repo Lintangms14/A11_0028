df9d6084fd2bd54b89b21098f754ae1fb9ef9a29
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 13:11:20 +0700

    untuk menampilkan tampilan detail pengembalian berdasarkan status dari DetailPengembalianUiState

--- app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/DetailPengembalianView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/DetailPengembalianView.kt
@@ -1,11 +1,14 @@
 package com.example.finalprojecta11.ui.view.pengembalian
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,10 +18,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.finalprojecta11.model.Peminjaman
 import com.example.finalprojecta11.model.Pengembalian
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 import com.example.finalprojecta11.ui.view.peminjaman.ComponentDetailAgt
+import com.example.finalprojecta11.ui.viewmodel.pengembalian.DetailPengembalianUiState
 
 object DestinasiDetailPemngembalian: DestinasiNavigasi {
     override val route = "detail pengembalian"
@@ -28,11 +31,50 @@ object DestinasiDetailPemngembalian: DestinasiNavigasi {
 }
 
 @Composable
+fun BodyDetailPngmbln(
+    modifier: Modifier = Modifier,
+    detailPengembalianUiState: DetailPengembalianUiState,
+    onDeleteClick: () -> Unit = { }
+) {
+    when (detailPengembalianUiState) {
+        is DetailPengembalianUiState.Loading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        is DetailPengembalianUiState.Success -> {
+            Column(
+                modifier = modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                ItemDetailPngmbln(
+                    pengembalian = detailPengembalianUiState.pengembalian,
+                    modifier = Modifier
+                )
+            }
+        }
+        is DetailPengembalianUiState.Error -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data Tidak Ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
 fun ItemDetailPngmbln(
     modifier: Modifier = Modifier,
     pengembalian: Pengembalian,
 ) {
-    Card (
+    Card(
         modifier = modifier
             .fillMaxWidth(),
         colors = CardDefaults.cardColors(
@@ -40,41 +82,45 @@ fun ItemDetailPngmbln(
             contentColor = MaterialTheme.colorScheme.onPrimaryContainer
         )
     ) {
-        Column (
+        Column(
             modifier = Modifier.padding(16.dp)
-        ){
+        ) {
             ComponentDetailAgt(judul = "Id Pengembalian", isinya = pengembalian.id_pengembalian)
             Spacer(modifier = Modifier.padding(4.dp))
 
             ComponentDetailAgt(judul = "Id Peminjaman", isinya = pengembalian.id_peminjaman)
             Spacer(modifier = Modifier.padding(4.dp))
 
-            ComponentDetailAgt(judul = "Tanggal Di kembalikan", isinya = pengembalian.tanggal_dikembalikan)
+            ComponentDetailAgt(
+                judul = "Tanggal Di kembalikan",
+                isinya = pengembalian.tanggal_dikembalikan
+            )
             Spacer(modifier = Modifier.padding(4.dp))
+        }
     }
-}
 
-@Composable
-fun ComponentDetailPngmbln(
-    modifier: Modifier = Modifier,
-    judul: String,
-    isinya: String
-) {
-    Column (
-        modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.Start
-    ){
-        Text(
-            text = "$judul : ",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Gray
-        )
+    @Composable
+    fun ComponentDetailPngmbln(
+        modifier: Modifier = Modifier,
+        judul: String,
+        isinya: String
+    ) {
+        Column(
+            modifier = modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = "$judul : ",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray
+            )
 
-        Text(
-            text = isinya,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-        )
+            Text(
+                text = isinya,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }
     }
 }
