d62233e20e4ddbe29bb5fca1eca6c15a54096dee
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:42:43 +0700

    untuk menampilkan daftar pengembalian dalam bentuk list

--- app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/HomePengembalianView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/HomePengembalianView.kt
@@ -1,16 +1,22 @@
 package com.example.finalprojecta11.ui.view.pengembalian
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.finalprojecta11.model.Pengembalian
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
@@ -18,6 +24,52 @@ import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 object DestinasiHomePengembalian : DestinasiNavigasi {
     override val route = "home pengembalian"
     override val titleRes = "Home pengembalian"
+}
+
+@Composable
+fun PngmblnLayout(
+    pengembalian: List<Pengembalian>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Pengembalian) -> Unit
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .border(BorderStroke(1.dp, MaterialTheme.colorScheme.onSurfaceVariant))
+                .padding(vertical = 8.dp, horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Id Pengembalian",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(1f)
+            )
+            Text(
+                text = "Id Peminjaman",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(2f)
+            )
+            Text(
+                text = "Tanggal Di kembalikan",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(2f)
+            )
+        }
+
+        LazyColumn(
+            contentPadding = PaddingValues(vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(pengembalian) { item ->
+                PngmblnTableRow(
+                    pengembalian = item,
+                    onDetailClick = onDetailClick
+                )
+            }
+        }
+    }
 }
 
 @Composable
