38e6321fe6a44a55803618e12ecb339e6c3a1640
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:06:03 +0700

    untuk menampilkan form untuk memperbarui data anggota

--- app/src/main/java/com/example/finalprojecta11/ui/view/anggota/UpdateAnggotaView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/anggota/UpdateAnggotaView.kt
@@ -3,7 +3,10 @@ package com.example.finalprojecta11.ui.view.anggota
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,12 +14,38 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 import com.example.finalprojecta11.ui.viewmodel.anggota.UpdateAnggotaUiEvent
+import com.example.finalprojecta11.ui.viewmodel.anggota.UpdateAnggotaUiState
 
 object DestinasiUpdateAnggota: DestinasiNavigasi {
     override val route = "update anggota"
     override val titleRes = "Update Anggota"
     const val id_anggota = "id_anggota"
     val routeWithArgs = "$route/{$id_anggota}"
+}
+
+@Composable
+fun UpdateBody(
+    updateAnggotaUiState: UpdateAnggotaUiState,
+    onValueChange: (UpdateAnggotaUiEvent) -> Unit,
+    onUpdateClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(26.dp)
+    ) {
+        UpdateFormInput(
+            updateAnggotaUiEvent = updateAnggotaUiState.updateAnggotaUiEvent,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onUpdateClick,
+            shape = MaterialTheme.shapes.small,
+        ) {
+            Text(text = "Update")
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
