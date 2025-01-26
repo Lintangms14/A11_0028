2bd9cb96fba93938c00acfa8035c3eb826a686bd
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:11:44 +0700

    untuk mendefinisikan rute dan judul untuk layar Home Peminjaman

---
+++ app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/HomePeminjamanView.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojecta11.ui.view.peminjaman
+
+import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+
+object DestinasiHomePeminjaman : DestinasiNavigasi {
+    override val route = "home peminjaman"
+    override val titleRes = "Home Peminjaman"
+}
