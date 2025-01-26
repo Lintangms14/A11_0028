2f2b3ff1dbaeee40c28c490191990dabf994dde5
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:12:45 +0700

    untuk menampilkan satu baris data peminjaman dalam bentuk tabel

--- app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/HomePeminjamanView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/HomePeminjamanView.kt
@@ -1,8 +1,62 @@
 package com.example.finalprojecta11.ui.view.peminjaman
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.finalprojecta11.model.Peminjaman
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 
 object DestinasiHomePeminjaman : DestinasiNavigasi {
     override val route = "home peminjaman"
     override val titleRes = "Home Peminjaman"
+}
+
+@Composable
+fun PmjmnTableRow(
+    peminjaman: Peminjaman,
+    onDetailClick: (Peminjaman) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onDetailClick(peminjaman) }
+            .border(BorderStroke(1.dp, MaterialTheme.colorScheme.onSurfaceVariant))
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = peminjaman.id_peminjaman,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = peminjaman.nama,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.weight(2f)
+        )
+        Text(
+            text = peminjaman.id_anggota,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.weight(2f)
+        )
+        Text(
+            text = peminjaman.tanggal_peminjaman,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.weight(2f)
+        )
+        Text(
+            text = peminjaman.tanggal_pengembalian,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.weight(2f)
+        )
+    }
 }
