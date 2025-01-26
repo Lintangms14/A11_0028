1b82059bdee96662526f81e78531f05d53305f18
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 11:34:34 +0700

    untuk mengonversi objek Pengembalian menjadi objek InsertPengembalianUiEvent

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/pengembalian/InsertPengembalianViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/pengembalian/InsertPengembalianViewModel.kt
@@ -2,6 +2,12 @@ package com.example.finalprojecta11.ui.viewmodel.pengembalian
 
 import com.example.finalprojecta11.model.Pengembalian
 
+fun Pengembalian.toInsertUiEvent(): InsertPengembalianUiEvent = InsertPengembalianUiEvent(
+    id_pengembalian = id_pengembalian,
+    id_peminjaman = id_peminjaman,
+    tanggal_dikembalikan = tanggal_dikembalikan
+)
+
 fun InsertPengembalianUiEvent.toPngmbln(): Pengembalian = Pengembalian(
     id_pengembalian = id_pengembalian,
     id_peminjaman = id_peminjaman,
