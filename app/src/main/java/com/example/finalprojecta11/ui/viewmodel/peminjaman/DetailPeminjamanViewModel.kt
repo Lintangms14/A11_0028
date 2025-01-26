00852dc049b029f9f573886a46a3ea1f424b829b
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 11:25:24 +0700

    untuk mengelola state dan interaksi dengan data terkait peminjaman di dalam aplikasi

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/peminjaman/DetailPeminjamanViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/peminjaman/DetailPeminjamanViewModel.kt
@@ -1,6 +1,64 @@
 package com.example.finalprojecta11.ui.viewmodel.peminjaman
 
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.finalprojecta11.model.Peminjaman
+import com.example.finalprojecta11.repository.PeminjamanRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+
+class DetailPeminjamanViewModel(private val Pmjmn: PeminjamanRepository) : ViewModel() {
+    private val _detailUiState = MutableStateFlow<DetailPeminjamanUiState>(DetailPeminjamanUiState.Loading)
+    val detailUiState: StateFlow<DetailPeminjamanUiState> = _detailUiState
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun getPeminjamanByid(id_peminjaman: String) {
+        viewModelScope.launch {
+            _detailUiState.value = DetailPeminjamanUiState.Loading
+            _detailUiState.value = try {
+                val peminjaman = Pmjmn.getPeminjamanByid(id_peminjaman)
+                DetailPeminjamanUiState.Success(peminjaman)
+            } catch (e: IOException) {
+                DetailPeminjamanUiState.Error
+            } catch (e: HttpException) {
+                DetailPeminjamanUiState.Error
+            }
+        }
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun updatePmjmn(id_peminjaman: String, updatedPeminjaman: Peminjaman) {
+        viewModelScope.launch {
+            try {
+                Pmjmn.updatePeminjaman(id_peminjaman, updatedPeminjaman)
+                _detailUiState.value = DetailPeminjamanUiState.Success(updatedPeminjaman)
+            } catch (e: IOException) {
+                _detailUiState.value = DetailPeminjamanUiState.Error
+            } catch (e: HttpException) {
+                _detailUiState.value = DetailPeminjamanUiState.Error
+            }
+        }
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun deletePmjmn(id_peminjaman: String) {
+        viewModelScope.launch {
+            try {
+                Pmjmn.deletePeminjaman(id_peminjaman)
+                _detailUiState.value = DetailPeminjamanUiState.Loading
+            } catch (e: IOException) {
+                _detailUiState.value = DetailPeminjamanUiState.Error
+            } catch (e: HttpException) {
+                _detailUiState.value = DetailPeminjamanUiState.Error
+            }
+        }
+    }
+}
 
 sealed class DetailPeminjamanUiState {
     data class Success(val peminjaman: Peminjaman) : DetailPeminjamanUiState()
