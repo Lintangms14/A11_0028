fff25f9c4098abbb769948219384973c4ce94ad4
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 10:33:31 +0700

    untuk mengelola logika bisnis dan status UI terkait dengan pengelolaan data anggota

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/anggota/HomeAnggotaViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/anggota/HomeAnggotaViewModel.kt
@@ -1,8 +1,56 @@
 package com.example.finalprojecta11.ui.viewmodel.anggota
 
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.finalprojecta11.model.Anggota
+import com.example.finalprojecta11.repository.AnggotaRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+
 sealed class HomeAnggotaUiState{
     data class Success(val anggota: List<Anggota>) : HomeAnggotaUiState()
     object Error : HomeAnggotaUiState()
     object Loading : HomeAnggotaUiState()
+}
+
+class HomeAnggotaViewModel(private val Agt: AnggotaRepository) : ViewModel() {
+    var AgtUiState: HomeAnggotaUiState by mutableStateOf(HomeAnggotaUiState.Loading)
+        private set
+
+    init {
+        getAgt()
+    }
+
+    fun getAgt() {
+        viewModelScope.launch {
+            AgtUiState = HomeAnggotaUiState.Loading
+            AgtUiState = try {
+                HomeAnggotaUiState.Success(Agt.getAnggota().data)
+            } catch (e: IOException) {
+                Log.e("GetAgtError", "IOException occurred: ${e.message}", e)
+                HomeAnggotaUiState.Error
+            } catch (e: HttpException) {
+                Log.e("GetAgtError", "HttpException occurred: ${e.message}", e)
+                HomeAnggotaUiState.Error
+            }
+        }
+    }
+
+
+    fun deleteAgt(id_anggota: String){
+        viewModelScope.launch {
+            try {
+                Agt.deleteAnggota(id_anggota)
+            } catch (e: IOException){
+                HomeAnggotaUiState.Error
+            } catch (e: HttpException){
+                HomeAnggotaUiState.Error
+            }
+        }
+    }
 }
