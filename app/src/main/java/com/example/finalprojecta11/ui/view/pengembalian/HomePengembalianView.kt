833d894688b1af5e64825cca06a4b89733892b86
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:40:21 +0700

    untuk menampilkan satu baris data pada tabel pengembalian

--- app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/HomePengembalianView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/HomePengembalianView.kt
@@ -1,8 +1,52 @@
 package com.example.finalprojecta11.ui.view.pengembalian
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.finalprojecta11.model.Pengembalian
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 
 object DestinasiHomePengembalian : DestinasiNavigasi {
     override val route = "home pengembalian"
     override val titleRes = "Home pengembalian"
+}
+
+@Composable
+fun PngmblnTableRow(
+    pengembalian: Pengembalian,
+    onDetailClick: (Pengembalian) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onDetailClick(pengembalian) }
+            .border(BorderStroke(1.dp, MaterialTheme.colorScheme.onSurfaceVariant))
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = pengembalian.id_pengembalian,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = pengembalian.id_peminjaman,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.weight(2f)
+        )
+        Text(
+            text = pengembalian.tanggal_dikembalikan,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.weight(2f)
+        )
+    }
 }
