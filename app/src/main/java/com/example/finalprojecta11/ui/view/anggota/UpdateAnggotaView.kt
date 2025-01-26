b1736b19be311daf87d19e308b724c517b22a9bf
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:07:39 +0700

    untuk menampilkan layar update dan untuk memperbarui data anggota

--- app/src/main/java/com/example/finalprojecta11/ui/view/anggota/UpdateAnggotaView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/anggota/UpdateAnggotaView.kt
@@ -1,20 +1,34 @@
 package com.example.finalprojecta11.ui.view.anggota
 
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalprojecta11.ui.customewidget.CostumeTopAppBar
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+import com.example.finalprojecta11.ui.viewmodel.PenyediaViewModel
 import com.example.finalprojecta11.ui.viewmodel.anggota.UpdateAnggotaUiEvent
 import com.example.finalprojecta11.ui.viewmodel.anggota.UpdateAnggotaUiState
+import com.example.finalprojecta11.ui.viewmodel.anggota.UpdateAnggotaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiUpdateAnggota: DestinasiNavigasi {
     override val route = "update anggota"
@@ -22,6 +36,53 @@ object DestinasiUpdateAnggota: DestinasiNavigasi {
     const val id_anggota = "id_anggota"
     val routeWithArgs = "$route/{$id_anggota}"
 }
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateAgtScreen(
+    id_anggota: String,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateAnggotaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(id_anggota) {
+        viewModel.loadAnggota(id_anggota)
+    }
+
+    val updateAnggotaUiState = viewModel.UpdateuiState
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiUpdateAnggota.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        UpdateBody(
+            updateAnggotaUiState = updateAnggotaUiState,
+            onValueChange = viewModel::updateState,
+            onUpdateClick = {
+                coroutineScope.launch {
+                    viewModel.updateAgt()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
 
 @Composable
 fun UpdateBody(
