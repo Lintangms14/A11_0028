d0ebce8ab83cea84f211e7ec53dc48c3422bf874
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 13:00:19 +0700

    untuk menampilkan layar utama dalam proses pengembalian

--- app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/InsertPengembalianView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/InsertPengembalianView.kt
@@ -4,22 +4,73 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalprojecta11.ui.customewidget.CostumeTopAppBar
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
-import com.example.finalprojecta11.ui.viewmodel.anggota.InsertAnggotaUiEvent
+import com.example.finalprojecta11.ui.view.anggota.DestinasiEntryAnggota
+import com.example.finalprojecta11.ui.viewmodel.PenyediaViewModel
+import com.example.finalprojecta11.ui.viewmodel.anggota.InsertAnggotaViewModel
 import com.example.finalprojecta11.ui.viewmodel.pengembalian.InsertPengembalianUiEvent
 import com.example.finalprojecta11.ui.viewmodel.pengembalian.InsertPengembalianUiState
+import com.example.finalprojecta11.ui.viewmodel.pengembalian.InsertPengembalianViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntryPengembalian: DestinasiNavigasi {
     override val route = "item_pengembalian"
     override val titleRes = "Insert Pengembalian"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryPngmblnScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertPengembalianViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntryAnggota.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){
+            innerPadding ->
+        EntryBody(
+            insertPengembalianUiState = viewModel.uiState,
+            onPengembaliaValueChange = viewModel::updateInsertPngmblnState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertPngmbln()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
 
 @Composable
