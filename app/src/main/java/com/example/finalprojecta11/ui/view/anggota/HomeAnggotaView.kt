22a30d0555606b5d0ff12d3df1a2447d7861bed3
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 11:41:22 +0700

    untuk mendefinisikan sebuah objek DestinasiHomeAnggota

---
+++ app/src/main/java/com/example/finalprojecta11/ui/view/anggota/HomeAnggotaView.kt
@@ -0,0 +1,9 @@
+package com.example.finalprojecta11.ui.view.anggota
+
+import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+
+object DestinasiHomeAnggota : DestinasiNavigasi {
+    override val route = "home anggota"
+    override val titleRes = "Home Anggota"
+}
+
