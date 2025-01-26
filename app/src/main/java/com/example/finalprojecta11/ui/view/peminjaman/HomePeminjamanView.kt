ab4450999632b3ad12230eb1435870652bd2949f
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:14:35 +0700

    untuk menampilkan daftar peminjaman dalam bentuk tabel yang terstruktur

--- app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/HomePeminjamanView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/HomePeminjamanView.kt
@@ -1,16 +1,22 @@
 package com.example.finalprojecta11.ui.view.peminjaman
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.finalprojecta11.model.Peminjaman
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
@@ -19,6 +25,63 @@ object DestinasiHomePeminjaman : DestinasiNavigasi {
     override val route = "home peminjaman"
     override val titleRes = "Home Peminjaman"
 }
+
+@Composable
+fun PmjmnLayout(
+    peminjaman: List<Peminjaman>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Peminjaman) -> Unit
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .border(BorderStroke(1.dp, MaterialTheme.colorScheme.onSurfaceVariant))
+                .padding(vertical = 8.dp, horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Id Peminjaman",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(1f)
+            )
+            Text(
+                text = "Nama",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(2f)
+            )
+            Text(
+                text = "Id Anggota",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(2f)
+            )
+            Text(
+                text = "Tanggal Peminjaman",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(2f)
+            )
+            Text(
+                text = "Tanggal Pengembalian",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.weight(2f)
+            )
+        }
+
+        LazyColumn(
+            contentPadding = PaddingValues(vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(peminjaman) { item ->
+                PmjmnTableRow(
+                    peminjaman = item,
+                    onDetailClick = onDetailClick
+                )
+            }
+        }
+    }
+}
+
 
 @Composable
 fun PmjmnTableRow(
