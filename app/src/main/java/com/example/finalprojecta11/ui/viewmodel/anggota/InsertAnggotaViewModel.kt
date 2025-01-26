a28ea319c761920e2a49ab53fbdcf8a6ba6caf48
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 10:36:37 +0700

    untuk mengonversi objek Anggota menjadi objek InsertAnggotaUiEvent

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/anggota/InsertAnggotaViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/anggota/InsertAnggotaViewModel.kt
@@ -2,6 +2,13 @@ package com.example.finalprojecta11.ui.viewmodel.anggota
 
 import com.example.finalprojecta11.model.Anggota
 
+fun Anggota.toInsertUiEvent(): InsertAnggotaUiEvent = InsertAnggotaUiEvent(
+    id_anggota = id_anggota,
+    nama = nama,
+    email = email,
+    nomor_telepon = nomor_telepon
+)
+
 fun InsertAnggotaUiEvent.toAgt(): Anggota = Anggota(
     id_anggota = id_anggota,
     nama = nama,
