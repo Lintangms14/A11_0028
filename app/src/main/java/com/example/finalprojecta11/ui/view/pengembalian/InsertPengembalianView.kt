9cfe4e240ae97ee732d5c5b227c4edfc81a0d5bf
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:58:30 +0700

    menampilkan form input untuk pengembalian dan tombol simpan

--- app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/InsertPengembalianView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/InsertPengembalianView.kt
@@ -3,18 +3,49 @@ package com.example.finalprojecta11.ui.view.pengembalian
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+import com.example.finalprojecta11.ui.viewmodel.anggota.InsertAnggotaUiEvent
 import com.example.finalprojecta11.ui.viewmodel.pengembalian.InsertPengembalianUiEvent
+import com.example.finalprojecta11.ui.viewmodel.pengembalian.InsertPengembalianUiState
 
 object DestinasiEntryPengembalian: DestinasiNavigasi {
     override val route = "item_pengembalian"
     override val titleRes = "Insert Pengembalian"
+}
+
+@Composable
+fun EntryBody(
+    insertPengembalianUiState: InsertPengembalianUiState,
+    onPengembaliaValueChange: (InsertPengembalianUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertPengembalianUiEvent = insertPengembalianUiState.insertPengembalianUiEvent,
+            onValueChange = onPengembaliaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(text = "Simpan")
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
