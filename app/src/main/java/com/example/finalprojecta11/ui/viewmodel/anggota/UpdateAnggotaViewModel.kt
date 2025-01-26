1041f4c8e7aaa9eb0733b8125c69438d8698dc48
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 11:00:57 +0700

    untuk mengonversi objek Anggota menjadi objek UpdateAnggotaUiState

--- app/src/main/java/com/example/finalprojecta11/ui/viewmodel/anggota/UpdateAnggotaViewModel.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/viewmodel/anggota/UpdateAnggotaViewModel.kt
@@ -2,6 +2,10 @@ package com.example.finalprojecta11.ui.viewmodel.anggota
 
 import com.example.finalprojecta11.model.Anggota
 
+fun Anggota.toUpdateUiState(): UpdateAnggotaUiState = UpdateAnggotaUiState(
+    updateAnggotaUiEvent = toUpdateUiEvent()
+)
+
 data class UpdateAnggotaUiState(
     val updateAnggotaUiEvent: UpdateAnggotaUiEvent = UpdateAnggotaUiEvent()
 )
