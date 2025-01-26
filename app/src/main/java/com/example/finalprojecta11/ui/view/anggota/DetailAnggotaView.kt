20630ac53aaa45cb9c8c53942e28f950c5be4c70
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 11:55:17 +0700

    untuk menampilkan detail informasi tentang anggota

--- app/src/main/java/com/example/finalprojecta11/ui/view/anggota/DetailAnggotaView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/anggota/DetailAnggotaView.kt
@@ -1,10 +1,16 @@
 package com.example.finalprojecta11.ui.view.anggota
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetailAnggota: DestinasiNavigasi {
@@ -13,6 +19,32 @@ object DestinasiDetailAnggota: DestinasiNavigasi {
     const val id_anggota = "id_anggota"
     val routeWithArgs = "$route/{$id_anggota}"
 }
+
+@Composable
+fun ComponentDetailAgt(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+) {
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ){
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
 
 @Composable
 private fun DeleteConfirmationDialog(
