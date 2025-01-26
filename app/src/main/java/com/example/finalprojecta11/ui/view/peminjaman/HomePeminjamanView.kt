6f8907533c38409d6010e38e938a2d067ef0d05f
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:15:25 +0700

    untuk menampilkan status error

--- app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/HomePeminjamanView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/HomePeminjamanView.kt
@@ -1,6 +1,7 @@
 package com.example.finalprojecta11.ui.view.peminjaman
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -12,18 +13,49 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.finalprojecta11.R
 import com.example.finalprojecta11.model.Peminjaman
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 
 object DestinasiHomePeminjaman : DestinasiNavigasi {
     override val route = "home peminjaman"
     override val titleRes = "Home Peminjaman"
+}
+
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.connection_error),
+            contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(
+            onClick = retryAction
+        ) {
+            Text(stringResource(R.string.retry))
+        }
+    }
 }
 
 @Composable
