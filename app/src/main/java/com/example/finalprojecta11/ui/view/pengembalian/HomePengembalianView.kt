d136c56069dcc704357ee7ff55ca79077c785573
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:39:45 +0700

    untuk mengelola navigasi ke layar Home Pengembalian

---
+++ app/src/main/java/com/example/finalprojecta11/ui/view/pengembalian/HomePengembalianView.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojecta11.ui.view.pengembalian
+
+import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+
+object DestinasiHomePengembalian : DestinasiNavigasi {
+    override val route = "home pengembalian"
+    override val titleRes = "Home pengembalian"
+}
