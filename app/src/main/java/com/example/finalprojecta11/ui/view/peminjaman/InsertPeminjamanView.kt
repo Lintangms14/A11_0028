74a001c89db582ca8810060b40268e90dff2871f
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 12:20:50 +0700

    untuk memasukkan data peminjaman baru

--- app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/InsertPeminjamanView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/peminjaman/InsertPeminjamanView.kt
@@ -3,7 +3,10 @@ package com.example.finalprojecta11.ui.view.peminjaman
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,10 +14,37 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
 import com.example.finalprojecta11.ui.viewmodel.peminjaman.InsertPeminjamanUiEvent
+import com.example.finalprojecta11.ui.viewmodel.peminjaman.InsertPeminjamanUiState
 
 object DestinasiEntryPeminjaman: DestinasiNavigasi {
     override val route = "item_peminjaman"
     override val titleRes = "Insert Peminjaman"
+}
+
+@Composable
+fun EntryBody(
+    insertPeminjamanUiState: InsertPeminjamanUiState,
+    onPeminjamanValueChange: (InsertPeminjamanUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertPeminjamanUiEvent = insertPeminjamanUiState.insertPeminjamanUiEvent,
+            onValueChange = onPeminjamanValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(text = "Simpan")
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
