a64a03feed996f3e816e4c19b5f95f6e96cf0ae2
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 10:22:08 +0700

    Untuk menampilkan layar pembaruan buku dan untuk mengedit informasi buku dan menyimpan

--- app/src/main/java/com/example/finalprojecta11/ui/view/buku/UpdateBukuView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/buku/UpdateBukuView.kt
@@ -1,20 +1,34 @@
 package com.example.finalprojecta11.ui.view.buku
 
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalprojecta11.ui.customewidget.CostumeTopAppBar
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+import com.example.finalprojecta11.ui.viewmodel.PenyediaViewModel
+import com.example.finalprojecta11.ui.viewmodel.buku.UpdateBukuViewModel
 import com.example.finalprojecta11.ui.viewmodel.buku.UpdateUiEvent
 import com.example.finalprojecta11.ui.viewmodel.buku.UpdateUiState
+import kotlinx.coroutines.launch
 
 
 object DestinasiUpdate: DestinasiNavigasi {
@@ -22,6 +36,52 @@ object DestinasiUpdate: DestinasiNavigasi {
     override val titleRes = "Update Buku"
     const val id_buku = "id_buku"
     val routeWithArgs = "$route/{$id_buku}"
+}
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateBkScreen(
+    id_buku: String,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateBukuViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(id_buku) {
+        viewModel.loadBuku(id_buku)
+    }
+
+    val updateUiState = viewModel.UpdateuiState
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiUpdate.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        UpdateBody(
+            updateUiState = updateUiState,
+            onValueChange = viewModel::updateState,
+            onUpdateClick = {
+                coroutineScope.launch {
+                    viewModel.updateBk()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
 
 @Composable
