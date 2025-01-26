b0021d6fff11c548540d07c17c273b8f507865fc
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-26 11:52:00 +0700

    untuk menampilkan layar entri data anggota

--- app/src/main/java/com/example/finalprojecta11/ui/view/anggota/InsertAnggotaView.kt
+++ app/src/main/java/com/example/finalprojecta11/ui/view/anggota/InsertAnggotaView.kt
@@ -4,22 +4,73 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalprojecta11.ui.customewidget.CostumeTopAppBar
 import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+import com.example.finalprojecta11.ui.viewmodel.PenyediaViewModel
 import com.example.finalprojecta11.ui.viewmodel.anggota.InsertAnggotaUiEvent
 import com.example.finalprojecta11.ui.viewmodel.anggota.InsertAnggotaUiState
+import com.example.finalprojecta11.ui.viewmodel.anggota.InsertAnggotaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntryAnggota: DestinasiNavigasi {
     override val route = "item_anggota"
     override val titleRes = "Insert Anggota"
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryAgtScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertAnggotaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntryAnggota.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){
+            innerPadding ->
+        EntryBody(
+            insertAnggotaUiState = viewModel.uiState,
+            onAnggotaValueChange = viewModel::updateInsertAgtState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertAgt()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
 
 @Composable
 fun EntryBody(
