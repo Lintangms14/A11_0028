2586e49196bb86d96555121ae74d9d43f169de57
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-27 22:04:39 +0700

    untuk memanggil tampilan dengan tema perpustakaan

--- app/src/main/java/com/example/finalprojecta11/MainActivity.kt
+++ app/src/main/java/com/example/finalprojecta11/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.finalprojecta11
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,16 +14,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.finalprojecta11.ui.theme.FinalProjectA11Theme
+import com.example.finalprojecta11.ui.viewmodel.PerpustakaanApp
 
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FinalProjectA11Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PerpustakaanApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
