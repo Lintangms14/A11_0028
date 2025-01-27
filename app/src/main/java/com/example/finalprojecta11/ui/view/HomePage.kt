8b29b161d46e9cb9fecc25a9623be60abdeb9a50
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-27 21:41:43 +0700

    untuk mendefinisikan sebuah halaman navigasi home page

---
+++ app/src/main/java/com/example/finalprojecta11/ui/view/HomePage.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojecta11.ui.view
+
+import com.example.finalprojecta11.ui.navigation.DestinasiNavigasi
+
+object HomePage : DestinasiNavigasi {
+    override val route = "HomePage"
+    override val titleRes = "Home Page"
+}
